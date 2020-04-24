1343b4847c5733f2f4b88ae737a1486ed9445d0d
ZhivkoZ <56478575+ZhivkoZ@users.noreply.github.com>
2020-04-24 21:51:29 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import {mergeTypes} from 'merge-graphql-schemas';
-import User from './User';
-import Student from './Student';
-
-const typeDefs = [User, Student];
-
-export default mergeTypes(typeDefs, {all: true});
