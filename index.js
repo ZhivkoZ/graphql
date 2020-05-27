f9fa2028429483b77a0698ada56441e600717a50
ZhivkoZ <56478575+ZhivkoZ@users.noreply.github.com>
2020-05-27 18:44:51 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import {mergeResolvers} from 'merge-graphql-schemas';
-
-import User from './User';
-import Student from './Student';
-
-const resolvers = [User, Student];
-
-export default mergeResolvers(resolvers);
